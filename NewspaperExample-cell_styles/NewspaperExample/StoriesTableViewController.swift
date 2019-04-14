98a7162c720001597e3d285cf2848f1740ca1141
Subhasis <>
2019-04-14 10:23:07 +0530

    testing the file

--- NewspaperExample-cell_styles/NewspaperExample/StoriesTableViewController.swift
+++ NewspaperExample-cell_styles/NewspaperExample/StoriesTableViewController.swift
@@ -12,7 +12,7 @@ struct Headline {
 class StoriesTableViewController: UITableViewController {
 
     var headlines = [
-        Headline(id: 1, title: "Subhasis", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim. Morbi convallis tincidunt quam eget bibendum. Suspendisse malesuada maximus ante, at molestie massa fringilla id.", image: "Apple"),
+        Headline(id: 1, title: "Subhasis", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim. Morbi convallis tincidunt quam eget bibendum. Suspendisse malesuada maximus ante, at  massa fringilla id.", image: "Apple"),
         Headline(id: 2, title: "Babulu", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non, finibus et diam. Morbi et felis a lacus pharetra blandit.", image: "Banana"),
         Headline(id: 3, title: "Subhasis - Babulu", text: "Aliquam egestas ultricies dapibus. Nam molestie nunc in ipsum vehicula accumsan quis sit amet quam. Sed vel feugiat eros.", image: "Cantaloupe"),
         ]
